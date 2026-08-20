37bd31b40f3057f1517a58778a9d9e904b89036a
hartween <elias05gutti@gmail.com>
2026-08-20 14:03:25 -0600

    algunos ejemplos de casting

--- programacion1/practicas/practicas propias/tonteria de practica #1/Main.java
+++ programacion1/practicas/practicas propias/tonteria de practica #1/Main.java
@@ -1,8 +1,27 @@
 public class Main{
 	public static void main(String [] args){
-		Bicho bicho1 = new Bicho();
+		
+		int numeroEntero = 4;
+		float numeroDecimal = 3.14f;
+		char caracterSimple = 'a';
+		String cadenaTexto = "135325";
 
-		bicho1.asignarNombre("juansito");
-		bicho1.atacar();
+
+		//casting
+
+		//de entero a flotante
+		float nuevoDecimal = (float)numeroEntero;
+		System.out.println("De 4 a 4.0: "+nuevoDecimal);
+		//de flotante a entero
+		int nuevoEntero = (int) numeroDecimal;
+		System.out.println("de 3.14 a 3: "+nuevoEntero);
+
+		//de texto a entero.
+		nuevoEntero = Integer.parseInt(cadenaTexto);
+		nuevoEntero -= 1;
+		System.out.println("deberia dar 135324");
+
+		System.out.println();
+
 	}
 }
